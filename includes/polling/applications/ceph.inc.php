9353d61fb4e35b258d24d2faa9e1c9b7eaed466b
Adam Simpson <Adam@CopyworldIT.com.au>
2019-02-07 12:01:06 +1030

    Update ceph application poller to interpret and store placement group states counts.

--- includes/polling/applications/ceph.inc.php
+++ includes/polling/applications/ceph.inc.php
@@ -14,7 +14,88 @@ if (!empty($agent_data['app'][$name])) {
         }
         list($section, $data) = explode('>', $section);
 
-        if ($section == "poolstats") {
+        if ($section == 'cephstatus') {
+
+        } else if ($section == 'pgstates') {
+            $rrd_def = RrdDefinition::make()
+                ->addDataset('creating', 'GAUGE', 0)
+                ->addDataset('activating', 'GAUGE', 0)
+                ->addDataset('active', 'GAUGE', 0)
+                ->addDataset('clean', 'GAUGE', 0)
+                ->addDataset('down', 'GAUGE', 0)
+                ->addDataset('scrubbing', 'GAUGE', 0)
+                ->addDataset('deep', 'GAUGE', 0)
+                ->addDataset('degraded', 'GAUGE', 0)
+                ->addDataset('inconsistent', 'GAUGE', 0)
+                ->addDataset('peering', 'GAUGE', 0)
+                ->addDataset('repair', 'GAUGE', 0)
+                ->addDataset('recovering', 'GAUGE', 0)
+                ->addDataset('forced_recovery', 'GAUGE', 0)
+                ->addDataset('recovery_wait', 'GAUGE', 0)
+                ->addDataset('recovery_toofull', 'GAUGE', 0)
+                ->addDataset('recovery_unfound', 'GAUGE', 0)
+                ->addDataset('backfilling', 'GAUGE', 0)
+                ->addDataset('forced_backfill', 'GAUGE', 0)
+                ->addDataset('backfill_wait', 'GAUGE', 0)
+                ->addDataset('backfill_toofull', 'GAUGE', 0)
+                ->addDataset('backfill_unfound', 'GAUGE', 0)
+                ->addDataset('incomplete', 'GAUGE', 0)
+                ->addDataset('stale', 'GAUGE', 0)
+                ->addDataset('remapped', 'GAUGE', 0)
+                ->addDataset('undersized', 'GAUGE', 0)
+                ->addDataset('peered', 'GAUGE', 0)
+                ->addDataset('snaptrim', 'GAUGE', 0)
+                ->addDataset('snaptrim_wait', 'GAUGE', 0)
+                ->addDataset('snaptrim_error', 'GAUGE', 0);
+
+            $fields = [
+                'creating' => '0',
+                'activating' => '0',
+                'active' => '0',
+                'clean' => '0',
+                'down' => '0',
+                'scrubbing' => '0',
+                'deep' => '0',
+                'degraded' => '0',
+                'inconsistent' => '0',
+                'peering' => '0',
+                'repair' => '0',
+                'recovering' => '0',
+                'forced_recovery' => '0',
+                'recovery_wait' => '0',
+                'recovery_toofull' => '0',
+                'recovery_unfound' => '0',
+                'backfilling' => '0',
+                'forced_backfill' => '0',
+                'backfill_wait' => '0',
+                'backfill_toofull' => '0',
+                'backfill_unfound' => '0',
+                'incomplete' => '0',
+                'stale' => '0',
+                'remapped' => '0',
+                'undersized' => '0',
+                'peered' => '0',
+                'snaptrim' => '0',
+                'snaptrim_wait' => '0',
+                'snaptrim_error' => '0',
+            ];
+
+            $rrd_name = ['app', $name, $app_id, 'pgstates'];
+
+            foreach (explode("\n", $data) as $line) {
+                if (empty($line)) continue;
+                list($state_name, $count) = explode(':', $line);
+                $fields[$state_name] = $count;
+            }
+
+            print "Ceph Placement Group States:\n";
+            d_echo($fields);
+
+            $metrics["pgstates"] = $fields;
+            $tags = compact('name', 'app_id', 'pgstates', 'rrd_name', 'rrd_def');
+            data_update($device, 'app', $tags, $fields);
+
+        } else if ($section == "poolstats") {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('ops', 'GAUGE', 0)
                 ->addDataset('wrbytes', 'GAUGE', 0)
